86f649c5ca61da07a16d16c166645734cacbaaf5
kotnikd3 <denis.kotnik@gmail.com>
2020-10-31 13:26:41 +0100

    Bug in Console.java, override

--- src/game/Console.java
+++ src/game/Console.java
@@ -3,8 +3,7 @@ package game;
 public class Console implements Output {
 
     @Override
-    public void println(String msg) {
-        System.out.println(msg);
-    }
+    public void println(String msg) { System.out.println(msg); }
+    @Override
     public void print(String msg) { System.out.print(msg); }
 }
